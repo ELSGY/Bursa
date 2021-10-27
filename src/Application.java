26bace52db4431b481c4c77d4d938ace3e38796a
flaviuscojocariu23 <flavius.cojocariu@elysian-software.com>
2021-10-27 19:36:22 +0300

    [Flavius] Created connection to database

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+import java.util.logging.Logger;
+
+public class Application {
+
+	private final static Logger LOG = Logger.getLogger(Application.class.getName());
+
+	public void startUp(){
+
+
+
+	}
+}
