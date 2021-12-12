841d73f8588c0ebc567a926c57ebb3146504ca78
flaviuscojocariu23 <flavius.cojocariu@elysian-software.com>
2021-12-12 18:41:06 +0200

    [Flavius] Optimizations

--- src/rabbitmq/Consumer.java
+++ src/rabbitmq/Consumer.java
@@ -8,6 +8,8 @@ import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 public class Consumer {
@@ -15,7 +17,7 @@ public class Consumer {
 	// Logger
 	ConnectionFactory factory = new ConnectionFactory();
 	private static final String QUEUE = "stock-transactions";
-	private String MESSAGE = "";
+	private final List<String> MESSAGE = new ArrayList<>();
 
 	public void consumeMessage(int nrMessages) throws IOException, TimeoutException {
 
@@ -27,8 +29,7 @@ public class Consumer {
 
 			while (nr < nrMessages) {
 				DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-					this.MESSAGE = new String(delivery.getBody(), StandardCharsets.UTF_8);
-					System.out.println("MESSAGE [" + delivery.hashCode() + "]: '" + MESSAGE + "'");
+					MESSAGE.add(new String(delivery.getBody(), StandardCharsets.UTF_8));
 				};
 
 				CancelCallback cancelCallback = System.out::println;
@@ -36,6 +37,11 @@ public class Consumer {
 				channel.basicConsume(QUEUE, true, deliverCallback, cancelCallback);
 				nr += 1;
 			}
+
+			for (int i = 0; i < MESSAGE.size(); i++) {
+				System.out.println("MESSAGE [" + (i + 1) + "]: '" + MESSAGE.get(i) + "'");
+			}
+
 		}
 	}
 }
